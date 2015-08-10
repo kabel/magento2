360c890c58ee5b09de765aea5eb95a45319a0fde
Kevin Abel <kevin.abel.0@gmail.com>
2015-08-09 23:09:56 -0500

    Update the Random generator to use zend-math
    The algorithms from zend-math are compatible and faster.

--- lib/internal/Magento/Framework/Math/Random.php
+++ lib/internal/Magento/Framework/Math/Random.php
@@ -5,6 +5,8 @@
  */
 namespace Magento\Framework\Math;
 
+use Zend\Math\Rand;
+
 /**
  * Random data generator
  */
@@ -30,38 +32,11 @@ class Random
      */
     public function getRandomString($length, $chars = null)
     {
-        $str = '';
         if (null === $chars) {
             $chars = self::CHARS_LOWERS . self::CHARS_UPPERS . self::CHARS_DIGITS;
         }
 
-        if (function_exists('openssl_random_pseudo_bytes')) {
-            // use openssl lib if it is installed
-            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
-                $bytes = openssl_random_pseudo_bytes(PHP_INT_SIZE);
-                $hex = bin2hex($bytes); // hex() doubles the length of the string
-                $rand = abs(hexdec($hex) % $lc); // random integer from 0 to $lc
-                $str .= $chars[$rand]; // random character in $chars
-            }
-        } elseif ($fp = @fopen('/dev/urandom', 'rb')) {
-            // attempt to use /dev/urandom if it exists but openssl isn't available
-            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
-                $bytes = @fread($fp, PHP_INT_SIZE);
-                $hex = bin2hex($bytes); // hex() doubles the length of the string
-                $rand = abs(hexdec($hex) % $lc); // random integer from 0 to $lc
-                $str .= $chars[$rand]; // random character in $chars
-            }
-            fclose($fp);
-        } else {
-            // fallback to mt_rand() if all else fails
-            mt_srand(10000000 * (double)microtime());
-            for ($i = 0, $lc = strlen($chars) - 1; $i < $length; $i++) {
-                $rand = mt_rand(0, $lc); // random integer from 0 to $lc
-                $str .= $chars[$rand]; // random character in $chars
-            }
-        }
-
-        return $str;
+        return Rand::getString($length, $chars);
     }
 
     /**
@@ -74,29 +49,10 @@ class Random
     public static function getRandomNumber($min = 0, $max = null)
     {
         if (null === $max) {
-            $max = mt_getrandmax();
-        }
-        $range = $max - $min + 1;
-        $offset = 0;
-
-        if (function_exists('openssl_random_pseudo_bytes')) {
-            // use openssl lib if it is installed
-            $bytes = openssl_random_pseudo_bytes(PHP_INT_SIZE);
-            $hex = bin2hex($bytes); // hex() doubles the length of the string
-            $offset = abs(hexdec($hex) % $range); // random integer from 0 to $range
-        } elseif ($fp = @fopen('/dev/urandom', 'rb')) {
-            // attempt to use /dev/urandom if it exists but openssl isn't available
-            $bytes = @fread($fp, PHP_INT_SIZE);
-            $hex = bin2hex($bytes); // hex() doubles the length of the string
-            $offset = abs(hexdec($hex) % $range); // random integer from 0 to $range
-            fclose($fp);
-        } else {
-            // fallback to mt_rand() if all else fails
-            mt_srand(mt_rand() + (100000000 * microtime()) % PHP_INT_MAX);
-            return mt_rand($min, $max); // random integer from $min to $max
+            $max = PHP_INT_MAX;
         }
 
-        return $min + $offset; // random integer from $min to $max
+        return Rand::getInteger($min, $max);
     }
 
     /**
